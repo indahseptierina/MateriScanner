7282523152077f6dc61891e7b56a3ee9850cbb44
indahseptierina <iseptierina@gmail.com>
2023-02-21 21:43:34 +0700

    indahse

---
+++ PertemuanScanner.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class PertemuanScanner{
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan Nomer = ");
+        int nomer = input.nextInt();
+        System.out.println("Nomer = " + nomer);
+
+    }
+}
